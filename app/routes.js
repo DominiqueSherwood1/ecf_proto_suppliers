30a7dba61dbf017d19f39357730e22562fa6335e
Dominique Sherwood <dominique.sherwood@education.gov.uk>
2020-10-18 14:57:26 +0100

    routes

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,22 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/find-school-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var findSchools = req.session.data['find-school']
+
+  // Check whether the variable matches a condition
+  if (findSchools == "network") {
+    // Send user to next page
+    res.redirect('/refine_list_search')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/results')
+  }
+
+})
+
+
 module.exports = router
