3cb2704207e563ef8875dddee4c4523440b37902
Dominique Sherwood <dominique.sherwood@education.gov.uk>
2020-10-19 10:49:56 +0100

    updating routes

--- app/routes.js
+++ app/routes.js
@@ -15,45 +15,9 @@ router.post('/it-two/find-school-answer', function (req, res) {
   }
   else {
     // Send user to ineligible page
-    res.redirect('//it-two/results-school'')
-  }
-
-})
-
-router.post('/it-two-b/find-school-answer', function (req, res) {
-
-  // Make a variable and give it the value from 'how-many-balls'
-  var findSchools = req.session.data['find-school']
-
-  // Check whether the variable matches a condition
-  if (findSchools == "network") {
-    // Send user to next page
-    res.redirect('/it-two-b/confirm-list')
-  }
-  else {
-    // Send user to ineligible page
-    res.redirect('/it-two-b/results-school')
-  }
-
-})
-
-router.post('/it-two-b/find-school-answer', function (req, res) {
-
-  // Make a variable and give it the value from 'how-many-balls'
-  var findSchools = req.session.data['find-school']
-
-  // Check whether the variable matches a condition
-  if (findSchools == "school-name") {
-    // Send user to next page
     res.redirect('/it-two/results-school')
   }
-  else {
-    // Send user to ineligible page
-    res.redirect('//it-two/results-school')
-  }
 
 })
-
-
 
 module.exports = router
